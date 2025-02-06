0b03465fb97905d9b64a298bf26d5ff5c3f4097a
Joe <64791505+jxf0ae6@users.noreply.github.com>
2025-02-06 20:17:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,7 @@
-// create web server to handle comments
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var app = express();
+//create web server
+const express = require('express');
+const router = express.Router();
 
-// Parse JSON (uniform resource locators) and form encoded data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Enable all CORS requests
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
-});
+//middleware
+const { requireSignin, isAuth } = require('../controllers/auth');
+const { userById } = require
