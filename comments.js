9dd2502e5154753bb544115686155642ac42025e
Joe <64791505+jxf0ae6@users.noreply.github.com>
2025-02-06 20:13:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,16 @@
-// create web server
-// 1. import http module
-const http = require('http');
-// 2. create server object
-const server = http.createServer((req, res) => {
-    // 3. create response
-    res.write('Hello World');
-    res.end();
+// create web server to handle comments
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+// Parse JSON (uniform resource locators) and form encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable all CORS requests
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
 });
-// 4. listen to server
-server.listen(3000);
-console.log('Listening on port 3000...');
-// 5. run server
-// node comments.js
-// open browser, enter localhost:3000
-// you should see Hello World
-// to stop server, press ctrl + c
-// 6. run server in background
-// node comments.js &
-// to stop server
-// killall node
-// 7. run server in foreground
-// fg
-// to stop server
-// ctrl + c
